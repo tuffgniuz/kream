2b6c5bcb9b09a948c2977abb5ce7b5333635d995
manu-febie <imanuelfebie@gmail.com>
2020-10-29 22:58:54 +0700

    Fixed @babel/preset-env module error

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-import * as BABYLONS from 'babylonjs'
+import * as BABYLON from 'babylonjs'
+
+const canvas = document.querySelector('#canvas')
+const engine = new BABYLON.Engine(canvas, true)
+
+const createScene = () => {
+    // create the scene space
+    const scene = new BABYLON.Scene(engine)
+    scene.clearColor = new BABYLON.Color3(1, 0, 1)
+    
+    return scene
+}
+
+const scene = createScene() // call the create scene function
+
+// register a render loop to repeatedly render the scene
+engine.runRenderLoop( () => {
+    scene.render()
+})
+
+// watch for browser/canvas resize events
+window.addEventListener('resize', () => {
+    engine.resize()
+})
