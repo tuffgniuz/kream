5fdf276fe2de2bf4de74bb37fff0b366a0a06e28
manu-febie <imanuelfebie@gmail.com>
2020-11-01 16:35:15 +0700

    Added ArcRotateCamera

--- src/main.js
+++ src/main.js
@@ -4,17 +4,20 @@ const engine = new BABYLON.Engine(stage, true)
 const createScene = () => {
     // create scene space
     const scene = new BABYLON.Scene(engine)
-    
-    // add camera and attach to scene
-    const camera = new BABYLON.ArcRotateCamera('freeCamera', 10, 5, -80, new BABYLON.Vector3(0, 10, 0), scene)
-    camera.attachControl(stage, true)
-    
-    // create light
+
     const hemisphericLight = new BABYLON.HemisphericLight('hemisphericLight', new BABYLON.Vector3(-10, 20, 10), scene)
 
-    // import sneaker model
     const sneaker = BABYLON.SceneLoader.ImportMesh('', './assets/', 'sneaker2.babylon', scene);
-
+    
+    // add camera and attach to scene
+    const camera = new BABYLON.ArcRotateCamera('freeCamera',
+            BABYLON.Tools.ToRadians(45),
+            BABYLON.Tools.ToRadians(45),
+            100,
+            BABYLON.Vector3.Zero(),
+            scene)
+    camera.attachControl(stage, true)
+    
     return scene
 }
 
