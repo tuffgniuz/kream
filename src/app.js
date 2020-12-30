8add12406ef72af33772f1ea6ec72dc319cf0e3b
Manu Febie <imanuelfebie@gmail.com>
2020-12-30 22:42:52 +0700

    IN PROGRESS: animate on click event

--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@ const engine = new BABYLON.Engine(stage, true)
 
 const createScene = () => {
     const scene = new BABYLON.Scene(engine)    
-    // scene.clearColor = new BABYLON.Color3(0, 0, 0);
 
     const light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(-10, 20, 10), scene)
 
@@ -20,7 +19,62 @@ const createScene = () => {
     // TRANSPARENT CANVAS
     scene.clearColor = new BABYLON.Color4(0,0,0,0.0000000000000001);
 
-    const sneaker = BABYLON.SceneLoader.ImportMesh('', '../assets/', 'nike-air.babylon', scene)
+    const animSole = new BABYLON.Animation('animSole', 'position.y', 30, BABYLON.Animation.ANIMATIONTYPE_SIZE,
+        BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT)
+
+    const animSoleFoam = new BABYLON.Animation('animSoleFoam', 'position.y', 30, BABYLON.Animation.ANIMATIONTYPE_SIZE,
+        BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT)
+
+    // import sneaker mesh
+    const sneaker = BABYLON.SceneLoader.ImportMeshAsync('', '../assets/', 'nike-air.babylon', scene)
+        .then( (result) => {
+            // get meshes by name
+            const airBubbleLeft = scene.getMeshByName('air_bubble_left')
+            const sole = scene.getMeshByName('sole')
+            const soleFoam = scene.getMeshByName('sole_foam')
+
+            const soleKeys = []
+            const soleFoamKeys = []
+
+            soleKeys.push({
+                frame: 0,
+                value: 0,
+            })
+
+            soleKeys.push({
+                frame: 30,
+                value: -10,
+            })
+
+            soleFoamKeys.push({
+                frame: 0,
+                value: 0,
+            })
+
+            soleFoamKeys.push({
+                frame: 30,
+                value: -5,
+            })
+
+
+            animSole.setKeys(soleKeys)
+            animSoleFoam.setKeys(soleFoamKeys)
+
+            sole.animations = []
+            sole.animations.push(animSole)
+            soleFoam.animations = []
+            soleFoam.animations.push(animSoleFoam)
+
+
+            scene.beginAnimation(sole, 0, 30)
+            scene.beginAnimation(soleFoam, 0, 30)
+    })
+
+    // sneaker.then((result) => {
+    //     console.log(scene.getMeshByName('air_bubble_left'))
+    // })
+
+    // sneaker.actionManger = new BABYLON.ActionManager(scene)
 
     return scene
 }
