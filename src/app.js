d13971545914fa0258ac6fb4be37ee6db8cbbb2e
Manu Febie <imanuelfebie@gmail.com>
2021-01-04 03:23:57 +0700

    Action bar for interaction with the mesh has been added

--- src/app.js
+++ src/app.js
@@ -4,6 +4,10 @@ import { Sneaker } from './mesh'
 class App {
 
     constructor() {
+        /*
+         * set initial app properties and start the main function
+         **/
+
         // get the canvas element from the html body
         this.canvas = document.querySelector("#stage")
         
@@ -28,27 +32,73 @@ class App {
 
     goToStage() {
         let scene = new Scene(this.engine)  
-        scene.clearColor = new Color4(0,0,0,0.0000000000000001)
-
-        const camera = new ArcRotateCamera(
-            'camera',
-            Tools.ToRadians(-60),
-            Tools.ToRadians(60),
-            70,
-            Vector3.Zero(),
-            scene)
-
         const light = new HemisphericLight('light', new Vector3(-10, 20, 10), scene)
-
+        const camera = new ArcRotateCamera('camera', Tools.ToRadians(-60), Tools.ToRadians(60), 70, Vector3.Zero(), scene)
+        
+        scene.clearColor = new Color4(0,0,0,0.0000000000000001) 
         camera.attachControl(this.canvas, true)
+
+        // camera behaviours
+        camera.lowerRadiusLimit = 50
+        camera.upperRadiusLimit = 120
+        camera.useBouncingBehavior = true
+        let cameraRotate = camera.useAutoRotationBehavior = true;
+        
 
         // load the sneaker mesh
         const sneaker = new Sneaker(scene)
-        
-        console.log(sneaker.getSole())
+
+        // get nav items from the action-bar by #id
+        const fabricGuard = document.querySelector('#fabric-guard')
+        const laces = document.querySelector('#laces')
+        const sole = document.querySelector('#sole')
+        const soleFoam = document.querySelector('#sole-foam')
+        const swoosh = document.querySelector('#swoosh')
+
+        // const rotate = document.querySelector('#rotate')
+
+        fabricGuard.addEventListener('click', e => {
+            sneaker.animateTop()
+            sneaker.animateSoleAndSoleFoam()
+        })
+
+        //--animate sole on click
+        sole.addEventListener('click', e => {
+            sneaker.animateSole()
+        })
+
+        soleFoam.addEventListener('click', e => {
+            sneaker.animateSoleAndSoleFoam()
+        })
+
+        // rotate.addEventListener('click', e => {
+        //     cameraRotate = false
+        // })
 
         // set the scene to the start scene
         this.scene = scene;
+    }
+
+    customLaces(sneakerMesh) {
+    }
+
+    customSole(sneakerMesh) {
+
+    }
+
+    customSoleFoam(sneakerMesh) {
+        /* Get's called when the user clicks on Sole Foam on the action-bar
+         * -> takes a sneaker object as the parameter
+         * -> animates the sole and sole_foam position.y negative direction 
+         **/
+    }
+
+    customSwoosh(sneakerMesh) {
+
+    } 
+
+    loadScreen() {
+
     }
 
 }
--- src/mesh.js
+++ src/mesh.js
@@ -1,92 +1,171 @@
 import { ActionManager, Animation, AnimationGroup, SceneLoader } from 'babylonjs'
 
 export class Sneaker {
-
+    /*
+     * NOTES:
+     * Interactities?
+     * 1. Look inside shoe?
+     *
+     *
+     * customizable parts
+     * 1. swooshes
+     * 2. laces
+     * 3. sole
+     * 4. sole Foam
+     * */
     constructor(scene) {
         this.scene = scene
+
         // sneaker mesh import
         this.sneaker = new SceneLoader.ImportMeshAsync('', '../assets/', 'nike-air.babylon', this.scene)
         this.sneaker.actionManager = new ActionManager(this.scene)
 
         // default frame rate is set to 20
-        this.frameRate = 20
+        this.frameRate = 30
+
+        // [X] fabric_trim
+        // [X] fabric_trim_front_right
+        // [X] fabric_trim_front_left
+        // f eyelets_right
+        // eyelets_left
+        // tonque_front
+        // tongue_label
+        // tonque_top
+        // laces
+        // fabric_heal_upper
+        // fabric_main_right
+        // fabric_main_left
+        // fabric_upper_mid
+        // fabric_upper_mid2_right
 
         // animations properties
-        // fabric_heal_upper
-        this.animfabricHealUpper = new Animation('animfabricHealUpper', 'position.y', this.frameRate, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT)
-        // fabric_main_right
-        this.animFabricMainRight = new Animation('animFabricMainRight', 'position.y', this.frameRate, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT)
-        // fabric_main_left
-        this.animFabricMainLeft = new Animation('animFabricMainLeft', 'position.y', this.frameRate, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT)
-        // fabric_trim
-        this.animFabricTrim = new Animation('animFabricTrim', 'position.y', this.frameRate, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT)
-        // fabric_trim_front_left
-        this.animFabricTrimFrontLeft = new Animation('animFabricTrimFrontLeft', 'position.y', this.frameRate, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT)
-        // fabric_trim_front_right
-        this.animFabricTrimFrontRight = new Animation('animFabricTrimFrontRight', 'position.y', this.frameRate, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT)
-        // swoosh_right
-        this.animSwooshRight = new Animation('animSwooshRight', 'position.y', this.frameRate, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT)
-        // swoosh_left
-        this.animSwooshLeft = new Animation('animSwooshLeft', 'position.y', this.frameRate, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT)
+        this.animAirBubbleLeft = new Animation('animAirBubbleLeft', 'position.y', this.frameRate, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT)
+        this.animAirBubbleRight = new Animation('animAirBubbleRight', 'position.y', this.frameRate, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT)
+        this.animFabricInsole = new Animation('animFabricInsole', 'position.y', this.frameRate, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT)
+        this.animSole = new Animation('animSole', 'position.y', this.frameRate, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT)
+        this.animSoleFoam = new Animation('animSoleFoam', 'position.y', this.frameRate, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT)
+
+        this.animFabricHealGuard = new Animation('animFabricHealGuard', 'position.z', this.frameRate, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT)
+        this.animFabricToeGuard = new Animation('animFabricToeGuard', 'position.z', this.frameRate, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT)
+
+        //--animation groups
+        this.fabricGuardAnimationGroup = new AnimationGroup('fabricGuardAnimationGroup')
+        this.soleAndSoleFoamAnimationGroup = new AnimationGroup('soleAndSoleFoamAnimationGroup')
+        this.sneakerTopAnimationGroup = new AnimationGroup('sneakerTopAnimationGroup')
+
     }
 
-    animateMeshes() {
-        this.sneaker
-            .then((result) => {
-                //--NODES
-                const fabricHealUpper = this.scene.getMeshByName('fabric_heal_upper')
-                const fabricMainRight = this.scene.getMeshByName('fabric_main_right')
-                const fabricMainLeft = this.scene.getMeshByName('fabric_main_left')
-                const fabricTrim = this.scene.getMeshByName('fabric_trim')
-                const fabricTrimFrontRight = this.scene.getMeshByName('fabric_trim_front_right')
-                const fabricTrimFrontLeft = this.scene.getMeshByName('fabric_trim_front_left')
-                const swooshRight = this.scene.getMeshByName('swoosh_right')
-                const swooshLeft = this.scene.getMeshByName('swoosh_left')
+    animateTop() {
 
-                // animation keys
-                const 
-            })
+        this.sneaker.then((result) => {
+            const animFabricMainRight = new Animation('animFabricMainRight', 'position.y', this.frameRate, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT)
+            const animFabricMainLeft = new Animation('animFabricMainLeft', 'position.y', this.frameRate, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT)
+            const animFabricTrim = new Animation('animFabricTrim', 'position.y', this.frameRate, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT)
+            const animFabricTrimFrontLeft = new Animation('animFabricTrimFrontLeft', 'position.y', this.frameRate, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT)
+            const animfabricHealUpper = new Animation('animfabricHealUpper', 'position.y', this.frameRate, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT)
+            const animFabricTrimFrontRight = new Animation('animFabricTrimFrontRight', 'position.y', this.frameRate, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT)
+            const animSwooshRight = new Animation('animSwooshRight', 'position.y', this.frameRate, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT)
+            const animSwooshLeft = new Animation('animSwooshLeft', 'position.y', this.frameRate, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT)
+
+            const fabricMainRight = this.scene.getMeshByName('fabric_main_right')
+            const fabricMainLeft = this.scene.getMeshByName('fabric_main_left')
+            const fabricTrim = this.scene.getMeshByName('fabric_trim')
+            const fabricTrimFrontRight = this.scene.getMeshByName('fabric_trim_front_right')
+            const fabricTrimFrontLeft = this.scene.getMeshByName('fabric_trim_front_left')
+            const fabricHealUpper = this.scene.getMeshByName('fabric_heal_upper')
+            const swooshRight = this.scene.getMeshByName('swoosh_right')
+            const swooshLeft = this.scene.getMeshByName('swoosh_left')
+
+            const keys = []
+
+            keys.push({ frame: 0, value: 0 })
+            keys.push({ frame: 15, value: 3 })
+            keys.push({ frame: 30, value: 6 })
+
+            animFabricMainRight.setKeys(keys)
+            animFabricMainLeft.setKeys(keys)
+            animFabricTrim.setKeys(keys)
+            animFabricTrimFrontRight.setKeys(keys)
+            animFabricTrimFrontLeft.setKeys(keys)
+            animfabricHealUpper.setKeys(keys)
+            animSwooshRight.setKeys(keys)
+            animSwooshLeft.setKeys(keys)
+
+            this.sneakerTopAnimationGroup.addTargetedAnimation(animFabricMainRight, fabricMainRight)
+            this.sneakerTopAnimationGroup.addTargetedAnimation(animFabricMainLeft, fabricMainLeft)
+            this.sneakerTopAnimationGroup.addTargetedAnimation(animFabricTrim, fabricTrim)
+            this.sneakerTopAnimationGroup.addTargetedAnimation(animFabricTrimFrontRight, fabricTrimFrontRight)
+            this.sneakerTopAnimationGroup.addTargetedAnimation(animFabricTrimFrontLeft, fabricTrimFrontLeft)
+            this.sneakerTopAnimationGroup.addTargetedAnimation(animfabricHealUpper, fabricHealUpper)
+            this.sneakerTopAnimationGroup.addTargetedAnimation(animSwooshRight, swooshRight)
+            this.sneakerTopAnimationGroup.addTargetedAnimation(animSwooshLeft, swooshLeft)
+
+            this.sneakerTopAnimationGroup.play()
+
+        })
     }
 
     animateSole() {
+
+        if (this.fabricGuardAnimationGroup.onAnimationGroupEndObservable || this.soleAndSoleFoamAnimationGroup.onAnimationGroupEndObservable) {
+            this.fabricGuardAnimationGroup.reset()
+            this.soleAndSoleFoamAnimationGroup.reset()
+        }
+
         this.sneaker
             .then((result) => {
                 const sole = this.scene.getMeshByName('sole')
-                const soleKeys = []
+        
+                const keys = []
 
-                soleKeys.push({ frame: 0, value: 0 })
-                soleKeys.push({ frame: 15, value: -3 })
-                soleKeys.push({ frame: 30, value: -6 })
+                keys.push({ frame: 0, value: 0 })
+                keys.push({ frame: 15, value: -3 })
+                keys.push({ frame: 30, value: -6 })
 
-                animSole.setKeys(soleKeys)
+                this.animSole.setKeys(keys)
 
-                this.getSoleAnimation().setKeys(soleKeys)
+                sole.animations = []
+                sole.animations.push(this.animSole)
 
-                this.scene.beginAnimation(this.getSoleAnimation(), 0, 30)
-            })
+                this.scene.beginAnimation(sole, 0, this.frameRate)
+            })    
     }
 
-    animeSoleFoam() {
-        // animate the y position of the sole_foam mesh
-        const animSoleFoam = new Animation('animeSoleFoam', 'position.y', this.frameRate, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT)
-
+    animateSoleAndSoleFoam(keyValue1 = -2, keyValue2 = -4) {
         this.sneaker
             .then((result) => {
+                const airBubbleLeft = this.scene.getMeshByName('air_bubble_left')
+                const airBubbleRight = this.scene.getMeshByName('air_bubble_right')
+                const fabricInsole = this.scene.getMeshByName('fabric_insole')
+                const sole = this.scene.getMeshByName('sole') 
                 const soleFoam = this.scene.getMeshByName('sole_foam')
-                const soleFoamKeys = []
 
-                soleFoamKeys.push({ frame: 0, value: 0 })
-                soleFoamKeys.push({ frame: 15, value: -3 })
-                soleFoamKeys.push({ frame: 30, value: -6 })
+                const keys = []
+        
+                keys.push({ frame: 0, value: 0 })
+                keys.push({ frame: 15, value: keyValue1 })
+                keys.push({ frame: 30, value: keyValue2 })
 
-                animSoleFoam.setKeys(soleFoamKeys)
+                this.animAirBubbleLeft.setKeys(keys)
+                this.animAirBubbleRight.setKeys(keys)
+                this.animSole.setKeys(keys)
+                this.animSoleFoam.setKeys(keys)
+                this.animFabricInsole.setKeys(keys)
 
-                soleFoam.animations = []
-                soleFoam.animations.push(animSoleFoam)
+                this.soleAndSoleFoamAnimationGroup.addTargetedAnimation(this.animAirBubbleLeft, airBubbleLeft)
+                this.soleAndSoleFoamAnimationGroup.addTargetedAnimation(this.animAirBubbleRight, airBubbleRight)
+                this.soleAndSoleFoamAnimationGroup.addTargetedAnimation(this.animFabricInsole, fabricInsole)
+                this.soleAndSoleFoamAnimationGroup.addTargetedAnimation(this.animSole, sole)
+                this.soleAndSoleFoamAnimationGroup.addTargetedAnimation(this.animSoleFoam, soleFoam)
 
-                this.scene.beginAnimation(soleFoam, 0, 30)
+                this.soleAndSoleFoamAnimationGroup.play()
             })
     }
+
+    applyTexture(texture) {
+
+    }
+
 }
 
 
